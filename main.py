0ca2b301cbf374b04104169ddbd1092eaf9b3206
mishz <mishzorikhin@gmail.com>
2021-12-10 01:34:43 +0500

    add README.md

--- main.py
+++ main.py
@@ -1,17 +1,2 @@
-import getpass
-
 import ATM
 import BankCustomer
-
-
-ATM1 = ATM.ATM("0001")
-
-person1, person2 = BankCustomer.BankCustomer("Соколов Константин Тимофеевич", "въезд Ладыгина, 50", "pucru6081@yopmail.com"),\
-                   BankCustomer.BankCustomer("Сорокина Елизавета Яновна", " бульвар Славы, 32", "kellie22@green.com")
-
-ATM1.crediting_funds(person2, 100)
-ATM1.transfer_another(person1, person2, 100)
-print(ATM1.get_balance(person1)[0])
-print(ATM1.get_balance(person2)[0])
-ATM1.withdrawal_funds(person2, -100)
-print(ATM1.get_balance(person2)[0])
