cec1473806969b1918de85fb42dd187dec2dce0d
mishz <mishzorikhin@gmail.com>
2021-12-10 11:21:46 +0500

    add README.md

--- main.py
+++ main.py
@@ -6,9 +6,6 @@ ATM1 = ATM.ATM("0001")
 person1 = BankCustomer.BankCustomer("Соколов Константин Тимофеевич", "въезд Ладыгина, 50", "pucru6081@yopmail.com")
 
 person1.ChangePIN(1234)
-print(person1)
-ATM1.crediting_funds(person1, 100)
-ATM1.crediting_funds(person1, 300)
-print(ATM1.get_balance(person1, "CurAcc"))
-print(*ATM1.display_transaction(person1))
 
+ATM1.replenishment_savings_account(person1,50)
+print(person1)
